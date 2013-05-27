032e846d483ecd6b775f18d583effb99cb6df37e
Matt Hughes <hughesm02@gmail.com>
2013-05-27 17:31:22 -0400

    refresh media for testing

--- mediagenerator/utils.py
+++ mediagenerator/utils.py
@@ -90,7 +90,7 @@ def get_media_url_mapping():
 
 def media_urls(key, refresh=False):
     if media_settings.MEDIA_DEV_MODE:
-        if refresh:
+        if refresh or _generated_names == {}:
             _refresh_dev_names()
         return [DEV_MEDIA_URL + url for url in _generated_names[key]]
     return [PRODUCTION_MEDIA_URL + get_production_mapping()[key]]
